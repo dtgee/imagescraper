266c2ccd9998e8829c3e28c621a86ba64cb0f486
dtgee <techan88@gmail.com>
2016-06-23 14:24:51 -0700

    fixed variable definitions and scopes in spider

--- imagescraper/spiders/imagespider.py
+++ imagescraper/spiders/imagespider.py
@@ -3,15 +3,18 @@ import datetime
 import scrapy
 
 class ImageSpider(scrapy.Spider):
-    name = "pyimagesearch-image-spider"
+    name = "image-spider"
     start_urls = ["https://www.reddit.com/r/pics"]
-    link_class = "thumbnail may-blank"
+    img_extensions = ('.jpg', '.jpeg', '.png', '.gif')
 
     def parse(self, response):
+        link_class = "\"thumbnail may-blank\""
+
         for url in response.css('a[class^=' + link_class + ']').xpath("@href").extract():
-        # TODO: grab image if it is already an image
-        # "Click" url if it is not an image
-            yield scrapy.Request(url, self.parse_page)
+            if url.endswith(self.img_extensions):
+                yield Image(file_urls=[img_url])
+            else:
+                yield scrapy.Request(url, self.parse_page)
 
     def parse_page(self, response):
         img_url = response.xpath("//img/@src").extract()
