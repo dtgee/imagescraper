3f933c0cbecf8db437b949d0a03b1d1b4b8baa4e
dtgee <techan88@gmail.com>
2016-06-28 20:49:33 -0700

    added database creation and added to pipeline.py

--- imagescraper/pipelines.py
+++ imagescraper/pipelines.py
@@ -5,7 +5,13 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import psycopg2
 
 class ImagescraperPipeline(object):
+
+    def __init__(self, postgres_uri, postgres_db):
+        self.postgres_uri = postgres_uri
+        self.postgres_db = postgres_db
+
     def process_item(self, item, spider):
         return item
