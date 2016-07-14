09f5cc3918ad3bc65d7bea74ec287ad1dccd2bf7
dtgee <techan88@gmail.com>
2016-07-14 11:49:55 -0700

    fix return of retrieve.py

--- reddit_gallery/client/retrieve.py
+++ reddit_gallery/client/retrieve.py
@@ -1,6 +1,6 @@
 import os
 import django
-from django.http import JsonResponse
+import json
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reddit_gallery.server.config.settings")
 django.setup()
 from reddit_gallery.server.gallery.models import Images
@@ -8,10 +8,9 @@ from reddit_gallery.server.gallery.serializers import ImagesSerializer
 
 def main(): 
     images = Images.objects.all()
-    serializer = ImagesSerializer(images)
-    #return "hello"
-    print serializer.data
-    print JsonResponse(serializer.data)
+    serializer = ImagesSerializer(images, many=True)
+    #print serializer.data
+    print json.dumps(serializer.data)
 
 if __name__ == '__main__':
     main()
