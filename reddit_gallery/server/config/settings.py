c4dac6ab773d1caa7b277d073b769563a6d4339f
dtgee <techan88@gmail.com>
2016-07-18 18:14:04 -0700

    install nginx and change config

--- reddit_gallery/server/config/settings.py
+++ reddit_gallery/server/config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reddit_gallery',
+    'gallery',
+    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -58,7 +60,9 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- reddit_gallery/server/urls.py
+++ reddit_gallery/server/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from gallery.views import ImagesViewSet
@@ -21,6 +21,7 @@ from gallery.views import ImagesViewSet
 router = DefaultRouter()
 router.register(prefix='images', viewset=ImagesViewSet)
 
-urlpatterns = [
-    url(r'^index/', include(router.urls),
-]
+urlpatterns = router.urls
+#urlpatterns = [
+#    url(r'^index/', include(router.urls)),
+#]
