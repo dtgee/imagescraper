7315d238dbbe70d03bb1b94d34f6578f341abeb8
dtgee <techan88@gmail.com>
2016-08-05 17:38:43 -0700

    fix settings and urls

--- reddit_gallery/server/gallery/views.py
+++ reddit_gallery/server/gallery/views.py
@@ -3,7 +3,7 @@ from gallery.models import Images
 from gallery.serializers import ImagesSerializer
 
 class ImagesViewSet(viewsets.ModelViewSet):
-""" ViewSet for viewing and editing Images objects """
+    """ ViewSet for viewing and editing Images objects """
     queryset = Images.objects.all()
     serializer_class = ImagesSerializer
 
